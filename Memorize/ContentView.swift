a99219dab420906fff3c9027613aa80b47c06157
PiaDue <pia.duenow@web.de>
2022-09-30 16:23:22 +0200

    add and remove cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View { //behaves like a view
-    var emojis:[String] = ["✈️", "🚲", "🚗", "🛵"]
+    var emojis:[String] = ["🚗", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚲", "🚚", "🚆", "🛴", "🛻"]
+    @State var emojiCount: Int = 4 //saved in memory
     
     var body: some View { //body view
-        return HStack{
-            ForEach(emojis, id: \.self, content: { emoji in //parameter
-                CardView(content: emoji)//initial value for that card specifically
-            })
+        VStack{
+            
+            HStack{ //horizontal Stack of cards
+                ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in //parameter
+                    CardView(content: emoji)//initial value for that card specifically
+                })
+            }
+            .padding()
+            .foregroundColor(.red)
+            
+            HStack{
+                addCard
+                Spacer()
+                removeCard
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+            
         }
-        .padding()
-        .foregroundColor(.red)
         
+    }
+    
+    var addCard: some View{
+        Button(action: {
+            if(emojiCount<emojis.count){emojiCount+=1}
+        }, label: {
+            Image(systemName: "plus.circle")
+            
+        })
+    }
+    
+    var removeCard: some View{
+        Button(action: {
+            if(emojiCount>1){emojiCount-=1}
+        }, label: {
+            Image(systemName: "minus.circle")
+            
+        })
     }
 }
 
@@ -62,6 +93,6 @@ struct CardView: View{
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
     }
 }
