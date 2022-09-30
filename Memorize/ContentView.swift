589227d6e25e4946671e7c442cc8141068eff544
PiaDue <pia.duenow@web.de>
2022-09-30 12:30:36 +0200

    created first card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView: View { //behaves like a view
+    var body: some View { //body view
+        return ZStack(content: {
+            
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth: 4)
+                .foregroundColor(.red)
+            
+            Text("Hello world!")
+                .foregroundColor(Color.blue)
+        })
+        .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
